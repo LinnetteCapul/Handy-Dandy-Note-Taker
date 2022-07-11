4a05b4c3eb6dca1546d081e39aaacc7cfae86c23
LinnetteCapul <linnette.capul@gmail.com>
2022-07-11 01:16:18 -0700

    added post route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -7,3 +7,29 @@ notes.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", (err, data) => res.json(JSON.parse(data)));
 });
 
+notes.post("/notes", (req, res) => {
+    console.info(`${req.method} request received to add a handy dandy note`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        fs.readFile("./db/db.json", (err, data) => {
+            let storedData = JSON.parse(data);
+            storedData.push(newNote);
+
+            fs.writeFile(`./db/db.json`, JSON.stringify(storedData), (err) =>
+                err
+                    ? console.error(err)
+                    : res.json(`Your handy dandy note has been written into JSON file`)
+            );
+        });
+    }
+});
+
+module.exports = notes;
